660c1b380f239de45a57b6ab0c0f4da26c1a5bf7
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-04-23 23:43:07 +0530

    Chart Completed

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import "package:intl/intl.dart";
 import '../models/transaction.dart';
 import '../functions/amountRetriver.dart';
+import '../widgets/bar.dart';
 
 class ChartWidget extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -16,9 +17,15 @@ class ChartWidget extends StatelessWidget {
       print(DateFormat.E().format(weekDay));
       print(totalAmountfortheDay);
       return {
-        'day': DateFormat.E().format(weekDay).substring(0,1),
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalAmountfortheDay
       };
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, tx) {
+      return sum + tx['amount'];
     });
   }
 
@@ -28,13 +35,21 @@ class ChartWidget extends StatelessWidget {
     return Card(
       elevation: 8,
       margin: EdgeInsets.all(10),
-      child: Row(
-        children: groupedTransactionValues.map(
-          (data) {
-            return Text('${data['day']}');
-          }
-        ).toList(),
-      ),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child:Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: groupedTransactionValues.map((data) {
+          return Flexible(
+            fit: FlexFit.tight,
+            child:BarChart(
+              data['day'],
+              data['amount'],
+              totalSpending == 0.0
+                  ? 0.0
+                  : (data['amount'] as double) / totalSpending));
+        }).toList(),
+      ),)
     );
   }
 }
